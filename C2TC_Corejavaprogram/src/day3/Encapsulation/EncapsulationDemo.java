c4298343a23d6f55fad96c58780c58afb480746f
hp <hp@DESKTOP-678DQDK>
2025-07-23 22:27:19 +0530

    This is my assignment

---
+++ C2TC_Corejavaprogram/src/day3/Encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package day3.Encapsulation;
+
+public class EncapsulationDemo {
+	public static void main(String[] args) {
+		
+		//object creation 
+		OopsConceptDemo obj = new OopsConceptDemo();
+		obj.setSerialNum(21);
+		obj.setName("Daisy");
+		obj.setAge(20);
+		System.out.println(obj);
+		
+	}
+
+}
