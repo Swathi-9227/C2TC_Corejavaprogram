ce09dd49a3d5df4c3bfd34280f6a2a73c7389712
hp <hp@DESKTOP-678DQDK>
2025-07-23 21:29:41 +0530

    This is my assignment2

---
+++ C2TC_Corejavaprogram/src/Assignment2/Main.java
@@ -0,0 +1,13 @@
+package Assignment2;
+
+public class Main {
+	public static void main(String[] args) {
+        // Create Student object
+        Student student = new Student();
+
+        // Create Commission object and use it
+        Commission emp = new Commission();
+        emp.acceptDetails();
+        emp.calculateCommission();
+    }
+}
