508a7027ceda956171060554e9f05f32f83cc52f
hp <hp@DESKTOP-678DQDK>
2025-07-29 21:41:40 +0530

    This is my assignment

---
+++ C2TC_Corejavaprogram/src/dayeight/interfaces/functionalinterfaces/GreetInterface.java
@@ -0,0 +1,8 @@
+
+//Program to demonstrate FunctionalInterface - Greet Interface
+package dayeight.interfaces.functionalinterfaces;
+
+@FunctionalInterface
+public interface GreetInterface {
+	public String greet();
+}
---
+++ C2TC_Corejavaprogram/src/dayeight/interfaces/markerinterfaces/Registrable.java
@@ -0,0 +1,7 @@
+
+//Program to demonstrate Marker Interface
+package dayeight.interfaces.markerinterfaces;
+
+public interface Registrable {
+
+}
