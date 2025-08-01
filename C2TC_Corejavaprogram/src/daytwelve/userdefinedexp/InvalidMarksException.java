5144a45210ce2d627df7e41677bfbad6e446717f
hp <hp@DESKTOP-678DQDK>
2025-08-01 20:08:48 +0530

    Day 12 Programs

---
+++ C2TC_Corejavaprogram/src/daytwelve/userdefinedexp/InvalidMarksException.java
@@ -0,0 +1,16 @@
+
+//Program to define User defined exception class
+package daytwelve.userdefinedexp;
+
+public class InvalidMarksException extends Exception{
+
+	public InvalidMarksException() {
+		super();
+	}
+
+	public InvalidMarksException(String message) {
+		super(message);
+	}
+
+	
+}
