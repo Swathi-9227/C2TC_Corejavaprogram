25ca47a0f09aad36c4f91671b8b6093e38816645
hp <hp@DESKTOP-678DQDK>
2025-08-06 20:04:48 +0530

    Day 21 Programs

---
+++ C2TC_Corejavaprogram/src/daytwentyone/userdefinedannotation/AndroidSeries.java
@@ -0,0 +1,15 @@
+
+//Program to demonstrate class to use user defined annotations 
+package daytwentyone.userdefinedannotation;
+
+@SmartTV(os = "android", height = 40, width = 50)
+@SmartPhone()
+class AndroidSeries {
+	String model;
+	int screenSize;
+
+	public AndroidSeries(String model, int screenSize) {
+		this.model = model;
+		this.screenSize = screenSize;
+	}
+}
